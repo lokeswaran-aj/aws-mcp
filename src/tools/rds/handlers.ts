a182aa33f85b9de34896636b8570d21275fc0eb9
Lokeswaran A <11lokeswaran@gmail.com>
2025-06-07 23:37:35 +0530

    feat: Update mocked rds handler with aws client

--- src/tools/rds/handlers.ts
+++ src/tools/rds/handlers.ts
@@ -1,11 +1,24 @@
+import { getRdsClient } from "@/aws-clients";
 import { RdsListInstancesArgs } from "@/types/rds";
+import { DescribeDBInstancesCommand } from "@aws-sdk/client-rds";
 
-export const listAllDbInstances = async (args: RdsListInstancesArgs) => {
+export const listAllDbInstances = async (
+  args: RdsListInstancesArgs
+): Promise<{ content: { type: "text"; text: string }[] }> => {
+  const rdsClient = getRdsClient({ region: args.region });
+  const command = new DescribeDBInstancesCommand();
+  const response = await rdsClient.send(command);
   return {
     content: [
       {
         type: "text" as const,
-        text: `Listing all RDS DB instances in region: ${args.region}:\n1. todo-app-db\n2. ecommerce-db\n3. blog-db`,
+        text: `${
+          response.DBInstances
+            ? `Listing all RDS DB instances in ${args.region} region:\n${response.DBInstances.map(
+                (instance) => `- ${JSON.stringify(instance, null, 2)}`
+              ).join("\n\n")}`
+            : "No RDS DB instances found"
+        }`,
       },
     ],
   };
