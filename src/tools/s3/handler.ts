ec400c7c2d914ab17cddb895be8f7b1f3f133fc0
Lokeswaran A <11lokeswaran@gmail.com>
2025-06-09 19:05:52 +0530

    feat: Add S3 tool and schema for deleting buckets

--- src/tools/s3/handler.ts
+++ src/tools/s3/handler.ts
@@ -1,8 +1,16 @@
 import { getS3Client } from "@/aws-clients";
-import { CreateBucketArgs, ListBucketsArgs } from "@/schema/s3";
+import {
+  CreateBucketArgs,
+  DeleteBucketArgs,
+  ListBucketsArgs,
+} from "@/schema/s3";
 import { HandlerReturnType } from "@/types/common";
 import { formatResponse } from "@/utils/format-response";
-import { CreateBucketCommand, ListBucketsCommand } from "@aws-sdk/client-s3";
+import {
+  CreateBucketCommand,
+  DeleteBucketCommand,
+  ListBucketsCommand,
+} from "@aws-sdk/client-s3";
 
 export const listAllBuckets = async (
   input: ListBucketsArgs
@@ -18,6 +26,15 @@ export const createBucket = async (
 ): Promise<HandlerReturnType> => {
   const s3Client = getS3Client({ region: input.region });
   const command = new CreateBucketCommand(input.S3Args);
+  const response = await s3Client.send(command);
+  return formatResponse(response);
+};
+
+export const deleteBucket = async (
+  input: DeleteBucketArgs
+): Promise<HandlerReturnType> => {
+  const s3Client = getS3Client({ region: input.region });
+  const command = new DeleteBucketCommand(input.S3Args);
   const response = await s3Client.send(command);
   return formatResponse(response);
 };
