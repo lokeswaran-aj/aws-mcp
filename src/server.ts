14735a9ad7e58ef891138488a50ab20fc6423aab
Lokeswaran A <11lokeswaran@gmail.com>
2025-06-15 13:32:24 +0530

    feat: Migrate MCP SDK to FastMCP

--- src/server.ts
+++ src/server.ts
@@ -1,38 +1,28 @@
-import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
-import { registerAmiTools } from "./tools/ami";
-import { registerEc2Tools } from "./tools/ec2";
-import { registerInstanceTagTools } from "./tools/instance-tag";
-import { registerInternetGatewayTools } from "./tools/internet-gateway";
-import { registerKeyPairTools } from "./tools/key-pair";
-import { registerRdsTools } from "./tools/rds";
-import { registerRouteTableTools } from "./tools/route-table";
-import { registerS3Tools } from "./tools/s3";
-import { registerSecurityGroupTools } from "./tools/security-group";
-import { registerSubnetTools } from "./tools/subnet";
+import { FastMCP } from "fastmcp";
 import { registerVpcTools } from "./tools/vpc";
 
-export const server = new McpServer(
-  {
-    name: "aws",
-    version: "0.0.1",
-    description: "AWS MCP Server",
-  },
-  {
-    capabilities: {
-      tools: {},
-    },
-  }
-);
+export const server = new FastMCP({
+  name: "aws",
+  version: "0.0.1",
+});
 
 // Register all tools
-void registerRdsTools(server);
-void registerS3Tools(server);
-void registerVpcTools(server);
-void registerSubnetTools(server);
-void registerInternetGatewayTools(server);
-void registerRouteTableTools(server);
-void registerSecurityGroupTools(server);
-void registerKeyPairTools(server);
-void registerEc2Tools(server);
-void registerAmiTools(server);
-void registerInstanceTagTools(server);
+// void registerRdsTools(server);
+// void registerS3Tools(server);
+void registerVpcTools();
+// void registerSubnetTools(server);
+// void registerInternetGatewayTools(server);
+// void registerRouteTableTools(server);
+// void registerSecurityGroupTools(server);
+// void registerKeyPairTools(server);
+// void registerEc2Tools(server);
+// void registerAmiTools(server);
+// void registerInstanceTagTools(server);
+
+void server.start({
+  httpStream: {
+    port: 8080,
+    endpoint: "/mcp",
+  },
+  transportType: "httpStream",
+});
